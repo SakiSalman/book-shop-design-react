6fcd473252ae324c50d72e158fbf33f1b1cba4d8
SakiSalman <devsix@neutrix.co>
2024-09-10 17:26:56 +0600

    fix: card grid design width

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -13,9 +13,9 @@ return (
     <div className="text-center mb-4 lg:text-start">
       <h1 className="text-xl md:text-2xl lg:text-2xl">বিষয়</h1>
     </div>
-    <div className="grid grid-cols-2 gap-2 md:grid-cols-3 lg:gap-4 lg:grid-cols-6 p-4 md:p-5 items-center">
+    <div className="grid grid-cols-2 gap-2 md:grid-cols-3 lg:gap-2 lg:grid-cols-6 p-4 md:p-5 items-center">
       {cards.map((item, index) => (
-        <div key={index} className="card w-full max-w-[162px] h-[165px] bg-gray-200 text-center flex flex-col items-center justify-center">
+        <div key={index} className="card w-full h-[165px] bg-gray-200 text-center flex flex-col items-center justify-center">
           <img className="object-cover w-full h-full" src={item.imageUrl} alt={`Card ${index + 1}`} />
           <div className="text mt-2">
             <h1 className='font-thin text-sm md:text-base'>{item.title}</h1>
@@ -33,7 +33,7 @@ return (
     </div>
     <div className="grid grid-cols-2 gap-2 md:grid-cols-3 lg:gap-4 lg:grid-cols-6 p-4 md:p-5 items-center">
       {cards.map((item, index) => (
-        <div key={index} className="card w-full max-w-[162px] h-[165px] bg-gray-200 text-center flex flex-col items-center justify-center">
+        <div key={index} className="card w-full h-[165px] bg-gray-200 text-center flex flex-col items-center justify-center">
           <img className="object-cover w-full h-full" src={item.imageUrl} alt={`Card ${index + 1}`} />
           <div className="text mt-2">
             <h1 className='text-[20px] text-sm md:text-base'>{item.title}</h1>
