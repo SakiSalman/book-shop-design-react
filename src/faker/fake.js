8684c698d06c785f7a6fc41783d86e3d5bec8042
Sami1234a <salman01705708434@gmail.com>
2024-09-10 21:13:20 +0600

    9:12PM 10/9/24 updated card

--- src/faker/fake.js
+++ src/faker/fake.js
@@ -1,15 +1,92 @@
+export const main = [
+  {
+    id: 1,
+    imageUrl:
+      "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Orthonitisastre_Darsaner_Daridra-Abul_Barakat-5ac7c-138091.jpg",
+    title: "ইসলামিক বই",
+  },
+  {
+    id: 2,
+    imageUrl:
+      "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/331fc10bb_176553.jpg",
+    title: "ইসলামিক বই",
+  },
+  {
+    id: 3,
+    imageUrl:
+      "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/850e279f4_79683.jpg",
+    title: "Card 3",
+  },
+  {
+    id: 4,
+    imageUrl:
+      "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/13276bc81_47097.jpg",
+    title: "Card 4",
+  },
+  {
+    id: 5,
+    imageUrl:
+      "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/30e858887_2225.jpg",
+    title: "Card 5",
+  },
+  {
+    id: 6,
+    imageUrl:
+      "https://ds.rokomari.store/rokomari110/ProductNew20…uvongkorer_Faki-Mohaimin_Patwari-4a19c-256250.jpg",
+    title: "Card 6",
+  },
+];
 export const cards = [
   {
     id: 1,
     imageUrl:
       "https://ds.rokomari.store/rokomari110/category/5aefd05f226f4_image.webp",
-    title: "Card 1",
+    title: "ইসলামিক বই",
   },
   {
     id: 2,
     imageUrl:
       "https://ds.rokomari.store/rokomari110/category/0abca295badd4_image.webp",
-    title: "Card 2",
+    title: "ইসলামিক বই",
+  },
+  {
+    id: 3,
+    imageUrl:
+      "https://ds.rokomari.store/rokomari110/category/fbc52375a1c64_image.webp",
+    title: "Card 3",
+  },
+  {
+    id: 4,
+    imageUrl:
+      "https://ds.rokomari.store/rokomari110/category/12322a1f91b14_image.webp",
+    title: "Card 4",
+  },
+  {
+    id: 5,
+    imageUrl:
+      "https://ds.rokomari.store/rokomari110/category/1d9f089d01a74_image.webp",
+    title: "Card 5",
+  },
+  {
+    id: 6,
+    imageUrl:
+      "https://ds.rokomari.store/rokomari110/category/16469bf2d8e64_image.png",
+    title: "Card 6",
+  },
+];
+
+export const superStore = [
+  {
+    id: 1,
+    imageUrl:
+      "https://www.rokomari.com/static/200/images/icons/home-popular-category/Serial14.png",
+    title: "ইসলামিক বই",
+  },
+  {
+    id: 2,
+    imageUrl:
+      "https://ds.rokomari.store/rokomari110/category/0abca295badd4_image.webp",
+    title: "ইসলামিক বই",
   },
   {
     id: 3,
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,45 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper/modules';
-import { cards } from '../../faker/fake';
+import { cards, main } from '../../faker/fake';
 
 const Home = () => {
  
 
 return (
   <>
+  <div className="_container  mt-4 flex-col items-center">
+  <div className="w-full px-4 pt-2 box-shadow">
+    <div className="text-center mb-4 lg:text-start ">
+      <h1 className="text-xl md:text-2xl lg:text-2xl lg:font-thin">আপনাদের জন্য </h1>
+    </div>
+    <div className="grid grid-cols-2 gap-2 md:grid-cols-3 lg:gap-4 lg:grid-cols-6 md:items-stretch   items-center">
+      {main.map((item, index) => (
+        <div key={index} className="card relative group  border-0 mb-4 w-full m-auto max-w-[217px] h-[348px] hover:border-2 hover:border-gray-400 text-center flex flex-col items-center justify-center">
+          <img className="object-cover " src={item.imageUrl} alt={`Card ${index + 1}`} />
+          <div className="text mt-2">
+            <h1 className='font-thin text-sm md:text-base'>{item.title}</h1>
+          </div>
+
+          <div className="button w-full h-full absolute text-center invisible flex flex-col group-hover:visible items-center justify-center">
+            <button className='translate-y-[-30px] font-bold font-mono rounded-md bg-[#f90] m-auto py-3 px-4 flex items-center justify-center '>Add To Card</button>
+
+           <button className='bg-slate-300 w-full p-2 font-bold text-blue-400'>View More</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
+</div>
+  {/* cards1 */}
   <div className="_container bg-white h- mt-3 flex flex-col items-center">
   <div className="w-full px-4 pt-2 box-shadow">
-    <div className="text-center mb-4 lg:text-start">
-      <h1 className="text-xl md:text-2xl lg:text-2xl">বিষয়</h1>
+    <div className="text-center mb-4 lg:text-start ">
+      <h1 className="text-xl md:text-2xl lg:text-2xl lg:font-thin">বেস্ট সেলার বিষয় </h1>
     </div>
-    <div className="grid grid-cols-2 gap-2 md:grid-cols-3 lg:gap-4 lg:grid-cols-6 p-4 md:p-5 items-center">
+    <div className="grid grid-cols-2 gap-3 gap-y-3 sm:grid-3 sm:gap-3 md:grid-cols-3 lg:gap-2 lg:grid-cols-6 p-4 md:p-5 items-center ">
       {cards.map((item, index) => (
-        <div key={index} className="card w-full max-w-[162px] h-[165px] bg-gray-200 text-center flex flex-col items-center justify-center">
+        <div key={index} className="card w-[165px] m-auto  h-[170px] bg-gray-200 text-center flex flex-col items-center justify-center">
           <img className="object-cover w-full h-full" src={item.imageUrl} alt={`Card ${index + 1}`} />
           <div className="text mt-2">
             <h1 className='font-thin text-sm md:text-base'>{item.title}</h1>
@@ -25,15 +49,15 @@ return (
     </div>
   </div>
 </div>
-
+{/* cards 2 */}
 <div className="_container bg-white h-auto mt-3 flex flex-col items-center">
   <div className="w-full px-4 pt-2 box-shadow">
     <div className="text-center mb-4 lg:text-start">
       <h1 className="text-xl md:text-2xl lg:text-3xl">Hello</h1>
     </div>
-    <div className="grid grid-cols-2 gap-2 md:grid-cols-3 lg:gap-4 lg:grid-cols-6 p-4 md:p-5 items-center">
+    <div className="grid grid-cols-2 gap-3 md:p-5  md:grid-cols-3 lg:gap-2 lg:grid-cols-6  p-4 items-center">
       {cards.map((item, index) => (
-        <div key={index} className="card w-full max-w-[162px] h-[165px] bg-gray-200 text-center flex flex-col items-center justify-center">
+        <div key={index} className=" m-auto card w-[165px] h-[179px] bg-gray-200 text-center flex flex-col items-center ">
           <img className="object-cover w-full h-full" src={item.imageUrl} alt={`Card ${index + 1}`} />
           <div className="text mt-2">
             <h1 className='text-[20px] text-sm md:text-base'>{item.title}</h1>
